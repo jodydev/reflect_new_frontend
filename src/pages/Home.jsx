d1a1410d073c7089602cf1cb5a72b2839c8ec9e4
Jody Ossino <jodyossino2001@gmail.com>
2024-09-20 21:12:24 +0200

    add animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Main from "../components/Main";
 import ScrollProgressButton from "../components/buttons/ScrollProgressButton";
 // import InitCustomCursor from '../components/cursor/InitCustomCursor';
-import Animations from "../components/Animations";
+// import Animations from "../components/Animations";
 import InfoSection from "../components/sections/InfoSection";
 import InfoSectionTwo from "../components/sections/InfoSectionTwo";
 import InfoSectionThree from "../components/sections/InfoSectionThree";
@@ -18,7 +18,7 @@ const Home = () => {
       {/* <Footer /> */}
       {/* <InitCustomCursor /> */}
       <ScrollProgressButton />
-      <Animations />
+      {/* <Animations /> */}
     </div>
   );
 };
