3244ec25e2c85481ccec0882ce00830e0ae7e6c1
Jody Ossino <jodyossino2001@gmail.com>
2024-09-20 20:50:38 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString(); // Divide i pacchetti per libreria
+            }
+          },
+        },
+      },
+    },
     plugins: [
       react()
     ],
@@ -10,9 +22,3 @@ export default defineConfig(() => {
 });
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()]
-// })
