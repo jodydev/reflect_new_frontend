20ef6e5cc1ddc482f5eda15086b415f471d2155e
Jody Ossino <jodyossino2001@gmail.com>
2024-09-20 14:21:00 +0200

    modify vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import vercel from 'vite-plugin-vercel';
  
 export default defineConfig({
   build: {
-    outDir: 'dist',  // La directory di output del build
-  },
-  ssr: {
-    noExternal: ['vite-plugin-vercel'],
+    outDir: 'dist',
   },
   plugins: [vercel()],
 });
