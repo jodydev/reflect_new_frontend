418a52d2d9ad9b87ac3a64f45576414049c42f80
Jody Ossino <jodyossino2001@gmail.com>
2024-09-21 16:52:09 +0200

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ export default function Hero() {
   const isMediumScreen = window.matchMedia("(min-width: 1024px)").matches;
 
   return (
-    <section className="relative my-10 md:my-4 h-96 md:h-full flex items-center">
-      <div className="relative z-10 w-full flex justify-center lg:justify-center">
+    <section className="relative my-10 md:my-4 h-96 md:h-full flex items-center lg:items-start">
+      <div className="relative z-10 w-full flex justify-center">
         <div className="container mx-auto px-4">
           <div className="flex flex-col lg:flex-row items-center md:gap-x-0 xl:gap-40">
             <div className="w-full mb-8 lg:mb-0 xl:mb-48">
